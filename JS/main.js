85de75070e4f7d14194ad7a2fa77e2403458c3ad
Rafaldruzba <rafaldruzba@op.pl>
2023-04-14 23:00:34 +0200

    x

--- JS/main.js
+++ JS/main.js
@@ -3,13 +3,13 @@ document.addEventListener('DOMContentLoaded', function(){
     const allNavItems = document.querySelectorAll('.nav-link')
     const navList = document.querySelector('.navbar-collapse')
 
-    function addShadow() {
-    if(window.scrollY >= 100) {
-        nav.classList.add('shadow-bg')
-        } else {
-            nav.classList.remove('shadow-bg')
-        }
-    }
+    // function addShadow() {
+    // if(window.scrollY >= 100) {
+    //     nav.classList.add('shadow-bg')
+    //     } else {
+    //         nav.classList.remove('shadow-bg')
+    //     }
+    // }
 
     allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
 
